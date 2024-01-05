36a53540bd494781b2af0da51498178c5a253e8e
Ella Budi Wijayanti <90185195+ellabw@users.noreply.github.com>
2024-01-05 19:11:32 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -112,6 +112,14 @@ tab1, tab2 = st.tabs(["Single-predict", "Multi-predict"])
 
 with tab1:
   st.sidebar.header("**User Input** Sidebar")
+  st.markdown(
+      """
+      <style>
+      .st-dl { background-color: #FFFF00; }
+      </style>
+      """,
+      unsafe_allow_html=True
+  )
 
   age = st.sidebar.number_input(label=":violet[**Age**]", min_value=df_final['age'].min(), max_value=df_final['age'].max())
   st.sidebar.write(f":orange[Min] value: :orange[**{df_final['age'].min()}**], :red[Max] value: :red[**{df_final['age'].max()}**]")
@@ -252,7 +260,16 @@ with tab1:
   st.subheader(result)
 
 with tab2:
-  st.header("Predict multiple data:")
+  st.header("predict multiple data:")
+
+  st.markdown(
+      """
+      <style>
+      .st-dl { background-color: #FFFF00; }
+      </style>
+      """,
+      unsafe_allow_html=True
+  )
 
   sample_csv = df_final.iloc[:5, :-1].to_csv(index=False).encode('utf-8')
 
